1c628a6ee6786a8445256b052002d848110e23f8
eric394163 <eric394163@gmail.com>
2023-12-11 14:31:07 +0900

    231211

---
+++ javaws/src/ex02/ifex.java
@@ -0,0 +1,13 @@
+package ex02;
+
+public class ifex {
+    public static void main(String[] args) {
+        int num1 = 0;
+        if ( num1 == 0){
+            System.out.println("0입니다.");
+        }else{
+            System.out.println("0이 아닙니다.");
+        }
+            
+    }
+}
---
+++ javaws/src/ex02/test02.java
@@ -0,0 +1,15 @@
+package ex02;
+
+public class test02 {
+    public static void main(String[] args) {
+        int num = 3;
+
+        if (num%2==0){
+            System.out.println("짝수");
+        }else if (num%2==1){
+            System.out.println("홀수");
+        }else{
+            System.out.println("에러");
+        }
+    }
+}
