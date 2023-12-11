28387e015e66fbe84ce4b187c725b297878d5ff4
eric394163 <eric394163@gmail.com>
2023-12-11 14:45:06 +0900

    231211

---
+++ javaws/src/ex02/elseifex.java
@@ -0,0 +1,21 @@
+package ex02;
+
+public class elseifex {
+    public static void main(String[] args) {
+        int num = 3;
+
+        if (num==0){
+            System.out.println("0");
+
+        }else if(num>0){
+            System.out.println("양수");
+        }else if(num<0){
+            System.out.println("음수");
+        }else{
+            System.out.println("에러");
+        }
+        
+        
+    }
+    
+}
